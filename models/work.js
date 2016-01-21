daf157962d2b8a0f1f68770049c8f856a31df586
James McCann <jmccnz@gmail.com>
2016-01-21 23:15:50 +1300

    Adds works model, add to homepage

---
+++ models/work.js
@@ -0,0 +1,19 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+var Work = new keystone.List('Work', {
+  autokey: { from: 'title', path: 'key', unique: true },
+  defaultSort: 'date',
+  defaultColumns: 'title, date'
+});
+
+Work.add({
+  title: { type: String, required: true, initial: true },
+  date: { type: Types.Date, index: true },
+  images: { type: Types.CloudinaryImages },
+  state: { type: Types.Select, options: 'draft, published, archived', default: 'draft' },
+  description: Types.Text,
+  createdAt: { type: Date, default: Date.now }
+})
+
+Work.register();
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,6 +1,22 @@
 var keystone = require('keystone');
+var Work = keystone.list('Work');
 
 module.exports = function(req, res) {
   var view = new keystone.View(req, res);
+
+  var locals = res.locals
+
+  view.on('init', function(next) {
+
+    Work.model.find()
+      .where('state', 'published')
+      .limit(4)
+      .exec(function(err, works) {
+        locals.works = [].concat(works)
+        next(err)
+      });
+
+  })
+
   view.render('index');
 };
